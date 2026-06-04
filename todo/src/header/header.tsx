7833f16d1ade5eb020316a77552e608c1166e984
Alice <alice.ozz.36@gmail.com>
2026-06-04 17:25:38 +0300

    Uploaded header

---
+++ todo/src/header/header.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import './header.css'
+
+export const Header = () => {
+    return (
+        <header>
+            <h1> הרשימה המבצעית </h1>
+        </header>
+    );
+}
+
